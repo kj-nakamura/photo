84ffd9421c2cce27328e799b2b099973ff6340d2
n.kenji <n.kenji@dayneeds.jp>
2019-07-29 15:49:27 +0900

    ログアウト

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,8 +1,20 @@
 <template>
   <footer class="footer">
-    <button class="button button--link">logout</button>
+    <button class="button button--link" @click="logout">logout</button>
     <RouterLink class="button button--link" to="/login">
       Login / Register
     </RouterLink>
   </footer>
 </template>
+
+<script>
+export default {
+  methods: {
+    async logout () {
+      await this.$store.dispatch('auth/logout')
+
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
