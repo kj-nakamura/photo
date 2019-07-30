27bcdcf3e8dcc36f3220fadc8affc7e3862a91fe
kenji nakamura <kenji.nkmr.1117@gmail.com>
2019-07-30 19:01:23 +0900

    ボタンの切り替え

---
+++ resources/js/components/PhotoForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-show="value" class="photo-form">
+    <h2 class="title">Submit a photo</h2>
+    <form class="form">
+      <input class="form__item" type="file">
+      <div class="form__button">
+        <button type="submit" class="button button--inverse">submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Boolean,
+            required: true
+        }
+    }
+}
+</script>
